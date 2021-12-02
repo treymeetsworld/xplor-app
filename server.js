9f3ad8e9a407645254008b0f76eb0cd3f719e9aa
Trevor Mwangi <trevorzac16@yahoo.com>
2021-12-02 10:38:55 -0500

    stub backend

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ import { fileURLToPath } from 'url'
 import logger from 'morgan'
 import cors from 'cors'
 
+import { router as usersRouter } from './routes/users.js'
+import { router as authRouter } from './routes/auth.js'
+import { router as tripsRouter } from './routes/trips.js'
+import { router as restaurantsRouter } from './routes/restraurants.js'
+import { router as attractionsRouter } from './routes/attractions.js'
+
 import('./config/database.js')
 
 const app = express()
 
-import { router as usersRouter } from './routes/users.js'
-import { router as authRouter } from './routes/auth.js'
 
 app.use(cors())
 app.use(logger('dev'))
@@ -18,6 +22,11 @@ app.use(express.json())
 
 app.use('/api/users', usersRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/trips', tripsRouter)
+app.use('/api/restaurants', restaurantsRouter)
+app.use('/api/attractions', attractionsRouter)
+
+
 
 app.get('/*', function (req, res) {
   res.sendFile(
