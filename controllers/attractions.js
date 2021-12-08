388b8f99395675d7edb4ecfdbe33d87ae2971975
Trevor Mwangi <trevorzac16@yahoo.com>
2021-12-08 04:32:28 -0500

    attractions

--- controllers/attractions.js
+++ controllers/attractions.js
@@ -1,11 +1,16 @@
 import { Trip } from '../models/trip.js'
 import { Attraction } from '../models/attraction.js'
+import axios from 'axios'
 
-function index(req, res) {
-  // User.find({})
-  // .then(users => res.json(users))
+function getAttractions(req,res) {
+  axios.get(`https://api.yelp.com/v3/businesses/search?location=${req.params.query}&term=attractions`,{
+    headers: {
+      'Authorization': `Bearer ${process.env.API_KEY}`, 'Content-Type': 'application/json'
+  },
+  })
+  .then(apiResponse => res.json(apiResponse.data))
 }
 
 export {
-  index,
+  getAttractions
 }
