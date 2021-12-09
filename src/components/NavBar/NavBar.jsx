20e12c2b8cb317a48f2b1f854f4a3278ea288257
C_Seckman <Cassie.seckman@gmail.com>
2021-12-09 12:23:42 -0500

    add nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,34 +5,44 @@ const NavBar = ({ user, handleLogout }) => {
 	return (
 		<>
 			{user ? (
-				<nav>
-					<div className="access">
-						<ul>
-							<li>
-								Welcome, {user.name}
-							</li>
-							<li>
-								<Link to='' onClick={handleLogout}>LOG OUT</Link>
-							</li>
-							<li>
-								<Link to='/profile' onClick="">Profile</Link>
-							</li>
-						</ul>
+				<nav class="navbar navbar-expand-lg">
+					<div class="container-fluid access">
+						<Link className="navbar-brand" to='/'>X'plor</Link>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNav">
+							<ul class="navbar-nav">
+								<li class="nav-item">
+									<Link className="navbar-brand" to='' onClick={handleLogout}>Log Out</Link>
+								</li>
+								<li class="nav-item">
+									<Link className="navbar-brand" to='/profile' onClick="">{user.name}'s Profile</Link>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</nav>
 			) : (
-				<nav>
-					<div>
-						<ul>
-							<li>
-								<Link to="/login" className="access">Log In</Link>
-							</li>
-							<li>
-								<Link to="/signup" className="access">Sign Up</Link>
-							</li>
-						</ul>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<div class="container-fluid access">
+						<Link className="navbar-brand" to='/'>X'plor</Link>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNav">
+							<ul class="navbar-nav">
+								<li class="nav-item">
+									<Link class="navbar-brand" to="/login" className="access">Log In</Link>
+								</li>
+								<li class="nav-item">
+									<Link class="navbar-brand" to="/signup" className="access">Sign Up</Link>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</nav>
+
 			)}
 		</>
 	)
